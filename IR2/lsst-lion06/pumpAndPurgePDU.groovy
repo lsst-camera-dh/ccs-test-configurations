3f115e6de482ff7d25ed7425f905e40c1b665e0f
CCS Operator Account <ccs@lsst-lion06.slac.stanford.edu>
2019-01-09 09:46:26 -0800

    Added pap-pdu groovy file and app file

---
+++ IR2/lsst-lion06/pumpAndPurgePDU.groovy
@@ -0,0 +1,34 @@
+import org.lsst.ccs.description.groovy.CCSBuilder;
+import org.lsst.ccs.subsystem.common.devices.power.distribution.*;
+import org.lsst.ccs.bootstrap.BootstrapResourceUtils;
+import org.lsst.ccs.subsystem.apcpdu.ApcPdu
+
+import org.lsst.ccs.monitor.Alarm;
+import org.lsst.ccs.monitor.Channel;
+
+CCSBuilder builder = ["pap-pdu"]
+
+Properties props = BootstrapResourceUtils.getBootstrapSystemProperties()
+def runMode = props.getProperty("org.lsst.ccs.run.mode","normal");
+
+String type = runMode.equals("simulation") ? "APC7900Sim" : "APC7900"
+
+builder.main (ApcPdu) {
+
+    PDU120 (APC7900Device, type: type, node:"192.168.1.2", outlets:["Outlet 1","Outlet 2","Outlet 3","Outlet 4","Outlet 5","Outlet 6","Outlet 7","Outlet 8"]
+ ) {
+        Power    (Channel, description:"PDU power", units:"Watts",
+                       hwChan:APC7900Device.CHAN_POWER, type:"POWER",
+                       limitHi:1320.0)
+
+    }
+
+    PDU230 (APC7900Device, type: type, node:"192.168.1.3", outlets:["Outlet 1","Outlet 2","Outlet 3","Outlet 4","Outlet 5","Outlet 6","Outlet 7","Outlet 8"]
+ ) {
+        Power    (Channel, description:"PDU power", units:"Watts",
+                       hwChan:APC7900Device.CHAN_POWER, type:"POWER",
+                       limitHi:1320.0)
+
+    }
+}
+
